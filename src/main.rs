a3be9f90eafe1e0b8c02cebc7af4775799a430ac
Dominik Völkel <voelkel.dominik@gmail.com>
2025-01-07 08:43:13 +0100

    works but wrong clock

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,6 @@ use stm32f0xx_hal::{pac, prelude::*};
 
 use defmt_rtt as _;
 
-use panic_probe as _;
-
 #[allow(clippy::empty_loop)]
 #[entry]
 fn main() -> ! {
@@ -24,14 +22,10 @@ fn main() -> ! {
         let mut led = cortex_m::interrupt::free(|cs| gpioa.pa4.into_push_pull_output(cs));
 
         loop {
-            // Turn PA1 on a million times in a row
-            for _ in 0..1_000 {
-                led.set_high().ok();
-            }
-            // Then turn PA1 off a million times in a row
-            for _ in 0..1_000 {
-                led.set_low().ok();
-            }
+            led.set_high().ok();
+            cortex_m::asm::delay(8_000_000);
+            led.set_low().ok();
+            cortex_m::asm::delay(8_000_000);
         }
     }
 
